51b97e14c715ff2df8f06eaf430b36470e331f4b
SaeidEmami <emami_na@hotmail.com>
2014-06-25 22:48:17 -0500

    Switched from GPLv3 to MIT License

--- surfUtils.py
+++ surfUtils.py
@@ -1,16 +1,8 @@
-# ##### BEGIN GNU/GPL LICENSE BLOCK #####
-#
-# License: This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or (at your
-# option) any later version. This program is distributed in the hope that it
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-# Public License for more details.
-#
-# ##### END GNU/GPL LICENSE BLOCK #####
-# Writer: Saeid Emami
-###############################################################################
+###################################################################################################
+# Available for use under the [MIT License](http://en.wikipedia.org/wiki/MIT_License)             #
+# Writer: Saeid Emami                                                                             #
+###################################################################################################
+
 
 '''
 Creates surfaces based on defined node sets on parts in Abaqus.
